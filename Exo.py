6feb3c02bbaa6beb568d13871cf38976a4df9ba9
Abou-sn <seckaboubacar1301@gmail.com>
2025-05-26 01:12:08 +0200

    Update depuis le pc du lycee.py

--- Exo.py
+++ Exo.py
@@ -1,7 +1,5 @@
 # Exo 1, Sujet n°01
 
-adj = [[1,2],[2],[0],[0]]
-
 def voisins_entrants(adj,x):
     # On crée une liste vide pour y mettre nos resulats
     resultat=[]
@@ -41,6 +39,36 @@ def max_et_indice(tab):
 
     return maximum,ind_max
 
+#Exo 2 sujet n°02
+
+def est_un_ordre (tab):
+    n = len(tab)
+    vus = []
+    for x in tab:
+        if x < 1 or x > n or x in vus:
+            return False
+        vus.append(x)
+    return True
+
+def nombre_points_rupture(ordre):
+    '''
+    Renvoie le nombre de point de rupture de ordre qui représente 
+    un ordre de gÃ¨nes de chromosome
+    '''
+    # on vérifie que ordre est un ordre de gènes
+    assert est_un_ordre(ordre)
+    n = len(ordre)
+    nb = 0
+    if ordre[0] != 1: # le premier n'est pas 1 
+        nb = nb + 1
+    i = 0
+    while i < n-1: 
+        if ordre[i]-ordre[i+1] not in [-1, 1]: # l'écart n'est pas 1 
+            nb = nb + 1
+        i = i + 1
+    if ordre[i] != n: # le dernier n'est pas n 
+        nb = nb + 1
+    return nb
 
 #Exo 1 sujet n°04
 
@@ -54,4 +82,59 @@ def intToBin(n):
 
     return binaire
 
-print(intToBin(64))
+# Exo 1 sujet n°12
+
+def fusion (l1,l2) :
+
+    listeA=[]
+    i=0
+    j=0
+    n1=len(l1)
+    n2=len(l2)
+
+    while i<n1 and j<n2 :
+        if l1[i]<l2[j]:
+            listeA.append(l1[i])
+            i+=1
+        else :
+            listeA.append(l2[j])
+            j+=1
+   
+    return listeA+l1[i:]+l2[j:]
+
+# Exo 2 sujet n°12
+romains = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500,"M":1000}
+def traduire_romain(nombre):
+    """ Renvoie l'écriture décimale du nombre donné en chiffres
+    romains """
+    if len(nombre) == 1:
+        return romains[nombre] 
+    elif romains[nombre[0]] >= romains[nombre[1]] : 
+        return romains[nombre[0]] + traduire_romain(nombre[1:]) 
+    else:
+        return traduire_romain(nombre[1:])-romains[nombre[0]] 
+
+# Exo 1 sujet 19
+
+def recherche_min(tab):
+    min = tab[0]
+    ind_min = 0
+    for i in range(len(tab)):
+        if tab[i]<min:
+            min = tab[i]
+            ind_min = i
+    return ind_min
+
+# Exo 2 sujet 19 
+def separe(tab):
+    '''Separe les 0 et les 1 dans le tableau tab'''
+    gauche = 0
+    droite = len(tab)-1 
+    while gauche < droite:
+        if tab[gauche] == 0 :
+            gauche = gauche+1 
+        else :
+            tab[gauche] = tab[droite] 
+            tab[droite] = 1 
+            droite = droite-1 
+    return tab
